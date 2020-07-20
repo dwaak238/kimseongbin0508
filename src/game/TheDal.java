cafcd6d0712dcde7abf30ff43e9e3835031f7ea3
김성빈 <kim968968@navdfj>
2020-07-20 14:42:08 +0900

    나누어 떨어지는 기능 추가

---
+++ src/game/TheDal.java
@@ -0,0 +1,141 @@
+package game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import vo.NumVO;
+
+public class TheDal {
+	// 맴버 변수
+	private Scanner sc;
+	private NumVO numVo;
+	private int op; // 더하기 빼기 곱하기 나누기를 정하는 변수
+	
+	
+//	private int num1;
+//	private int num2;
+	
+	
+	// 생성자
+	public TheDal() {
+		sc = new Scanner(System.in);
+		numVo = new NumVO();
+	}
+	
+	// num1 값 변경 하는 기능
+//	public void num1(int num1) {
+//		this.num1 = num1;
+//	}
+	
+//	// num1 값을 전달 해주는 기능
+//	public void num1() {
+//		
+//	}
+	
+	// 메뉴 출력
+	public void menu() {
+		System.out.print("1: 더하기, 2:빼기, 3:곱하기, 4:나누기, 0:종료 >> ");
+		op = sc.nextInt();
+		System.out.println("당신이 선택한 번호 = " + op);
+	}
+	
+	// 랜덤 숫자 만들기 매소드
+	public void rNum() {
+		Random rnd = new Random();
+//		num1 = rnd.nextInt(10);
+		numVo.setNum1(rnd.nextInt(89)+10);
+		
+//		num2 = rnd.nextInt(10);
+		numVo.setNum2(rnd.nextInt(89)+10);
+	}
+	
+	// 문제 만들기
+	public void matter() {
+		// 문제 출력
+		rNum(); // 랜덤 숫자 만들기 매소드
+		
+		if (op == 1) {
+			int dab = add();
+			compare(dab);
+		}
+		else if (op == 2) {
+			int dab = sub();
+			compare(dab);
+			}
+		else if (op == 3) {
+			int dab = mul();
+			compare(dab);
+			}
+		else if (op == 4) {
+			int dab = div();
+			compare(dab);
+			}
+	}
+	
+	// 빼기 또는 나누기일때 num1과 num2를 바꾸는 매소드
+	public void change(int a, int b) {
+		numVo.setNum1(Math.max(a, b));
+		numVo.setNum2(Math.min(a, b));
+	}
+	
+	// 나누기를 무조건 떨어지게 만들기
+	public boolean ddul(int a, int b) {
+		if (a % b == 0) {
+			return true;
+		}
+			return false;
+		
+	}
+		
+		// 답 입력 및 비교
+	public void compare(int dab) {
+//		int dab = numVo.getNum1() + numVo.getNum2(); 
+		
+		// 답 입력 받기
+		int i = sc.nextInt();
+		System.out.println("당신이 선택한 답 = " + i);
+		
+		// 만약에 사용자의 답과 비교해서
+		// 맞으면 정답 틀리면 오답
+		if (i == dab) {
+			System.out.println("정답입니다.");
+		}
+		else {
+			System.out.println("오답입니다.");
+		}
+	}
+
+	// 더하기
+	public int add() {
+		System.out.print(numVo.getNum1() + "+" + numVo.getNum2() + "= ");
+		return numVo.getNum1() + numVo.getNum2();
+	}
+
+	// 빼기
+	public int sub() {
+		change(numVo.getNum1(), numVo.getNum2());
+		System.out.print(numVo.getNum1() + "-" + numVo.getNum2() + "= ");
+		return numVo.getNum1() - numVo.getNum2();
+	}
+
+	// 곱하기
+	public int mul() {
+		System.out.print(numVo.getNum1() + "x" + numVo.getNum2() + "= ");
+		return numVo.getNum1() * numVo.getNum2();
+	}
+
+	// 나누기
+	public int div() {
+		for(;;) {
+		change(numVo.getNum1(), numVo.getNum2());
+		if (ddul(numVo.getNum1(), numVo.getNum2())) {
+			break;
+			}
+		else {
+			rNum();
+		}
+		}
+		System.out.print(numVo.getNum1() + "÷" + numVo.getNum2() + "= ");
+		return numVo.getNum1() / numVo.getNum2();
+	}
+}
---
+++ src/vo/NumVO.java
@@ -0,0 +1,32 @@
+package vo;
+
+public class NumVO {
+	private int num1;
+	private int num2;
+	
+	// Alt + Shift + s
+	public int getNum1() {
+		return num1;
+	}
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	
+	
+
+//	// num1 값 변경 하는 기능
+//	public void num1(int num1) {
+//		this.num1 = num1;
+//	}
+//
+//	// num1 값을 전달 해주는 기능
+//	public int num1() {
+//		return num1;
+//	}
+}
